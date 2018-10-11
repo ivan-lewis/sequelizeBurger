7ec5976003aef8c3e115d64a17498edc33185961
Ivan Lewis <ivan.lewis@tonation-nsn.gov>
2018-10-11 12:07:08 -0700

    adding more files

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Require models
+var db = require("./models");
+
+// Define express as our server and render the content in the public folder
+var app = express();
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// listen on port 3000
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening on port:", PORT);
+  });
+});
